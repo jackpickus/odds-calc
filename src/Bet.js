cdfe82c97ef9dc63f792186be47b54540acf9e85
Jack <jpick329@gmail.com>
2022-04-04 22:04:41 -0500

    created Bet class

---
+++ src/Bet.js
@@ -0,0 +1,10 @@
+export class Bet {
+  constructor(wager, units, odds, pWinnings, win, theBet) {
+    this.wager = wager;
+    this.units = units;
+    this.odds = odds;
+    this.pWinnings = pWinnings;
+    this.win = win;
+    this.theBet = theBet;
+  }
+}
